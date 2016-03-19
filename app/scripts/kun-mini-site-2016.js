1342ee86d168ce59002bd4597a80977981c09a3b
DavidPham <DavidPham>
2016-03-19 17:11:56 +0700

    First commit

---
+++ app/scripts/kun-mini-site-2016.js
@@ -0,0 +1,88 @@
+'use strict';
+var kunMini2016 = window.kunMini2016 || {}; //global namespace for YOUR kunMini2016, Please change kunMini2016 to your kunMini2016 name
+
+var isMobile = {
+    isAndroid: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    isBlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    isiOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    isOpera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    isWindows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (isMobile.isAndroid() || isMobile.isBlackBerry() || isMobile.isiOS() || isMobile.isOpera() || isMobile.isWindows());
+    }
+};
+
+(function($) {
+    kunMini2016.Global = {
+        init: function() { //initialization code goes here
+            $.support.cors = true;
+            this.initFormElements();
+        },
+
+        initFormElements: function() {
+            $('input, textarea').placeholder(); //enable placeholder support for all browsers
+
+            //Radio Wrapper
+            $(".radio-wrapper .input-radio").each(function() {
+                if ($(this).is(":checked")) {
+                    $('.input-radio[name="' + $(this).attr('name') + '"]').parents(".radio-selected").removeClass("radio-selected");
+                    $(this).parents('.radio-wrapper').addClass("radio-selected");
+                }
+            });
+
+            $(document).on('change', ".radio-wrapper .input-radio", function() {
+
+                $('input[name="' + $(this).attr('name') + '"]').each(function() {
+                    if ($(this).not(':checked')) {
+                        $(this).parent().removeClass("radio-selected");
+                    }
+                });
+
+                if ($(this).is(":checked")) {
+                    $(this).parents('.radio-wrapper').addClass("radio-selected");
+                }
+            });
+
+            //Checkbox Wrapper
+            $('.checkbox-wrapper .input-checkbox').each(function() {
+                if ($(this).is(':checked')) {
+                    $(this).parents('.checkbox-wrapper').addClass('checked');
+                }
+            });
+
+            $(document).on('click', '.checkbox-wrapper .input-checkbox', function() {
+
+                if ($(this).is(':checked')) {
+                    $(this).parents('.checkbox-wrapper').addClass('checked');
+                } else if ($(this).not(':checked')) {
+                    $(this).parents('.checkbox-wrapper').removeClass('checked');
+                }
+            });
+
+            //Select Wrapper
+            $('.select-wrapper').each(function() {
+                if ($(this).find('span').length <= 0) {
+                    $(this).prepend('<span>' + $(this).find('select option:selected').text() + '</span>');
+                }
+            });
+
+            $(document).on('change', '.select-wrapper select', function() {
+                $(this).prev('span').replaceWith('<span>' + $(this).find('option:selected').text() + '</span>');
+            });
+        }
+    };
+})(jQuery);
+
+$(document).ready(function($) {
+    kunMini2016.Global.init();
+});
