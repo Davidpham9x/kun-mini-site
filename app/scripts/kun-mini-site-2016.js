7491a26ee7559b218342884af238101bf0b0d440
DavidPham <DavidPham>
2016-03-20 12:00:20 +0700

    Update layout responsive

--- app/scripts/kun-mini-site-2016.js
+++ app/scripts/kun-mini-site-2016.js
@@ -27,6 +27,7 @@ var isMobile = {
         init: function() { //initialization code goes here
             $.support.cors = true;
             this.initFormElements();
+            this.initSliderCollectionVR();
         },
 
         initFormElements: function() {
@@ -79,6 +80,41 @@ var isMobile = {
             $(document).on('change', '.select-wrapper select', function() {
                 $(this).prev('span').replaceWith('<span>' + $(this).find('option:selected').text() + '</span>');
             });
+        },
+
+        initSliderCollectionVR: function () {
+            var divContent = $('.wrap-collect-toy');
+
+                divContent.find('.list-collection').slick({
+                    infinite: true,
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    responsive: [
+                        /*{
+                          breakpoint: 1024,
+                          settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: true
+                          }
+                        },*/
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 1
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1
+                            }
+                        }
+                    ]
+                });
         }
     };
 })(jQuery);
